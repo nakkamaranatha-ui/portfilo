2005813b2101e67a16faa76674fc7aece8a4f572
nakkamaranatha-ui <nakkamaranatha@gmail.com>
2026-05-23 19:42:44 +0530

    Add files via upload

---
+++ 6.js
@@ -0,0 +1,8 @@
+let age = prompt("What is your age?");
+if (age < 18) {
+   alert("You are a minor.");
+} else if (age >= 18 && age < 65) {
+   alert("You are an adult.");
+} else {
+ alert("You are a senior.");
+}
